2bd6362eb77216fa9e7aa45561d6a4fe2a58a5af
POURRI <pourri@mail.ru>
2013-11-17 01:12:51 +0200

    netBeans project files

---
+++ Projects/Java/FirstHid/src/firsthid/FirstHid.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package firsthid;
+
+/**
+ *
+ * @author POURRI
+ */
+public class FirstHid {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
