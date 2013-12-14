de9a53eaa4cb3ae1a4a48c91ff09476d7f15b15b
POURRI <pourri@mail.ru>
2013-12-14 16:10:22 +0200

    add need files to config
    but need modify search path

--- Projects/Delphi/main/MainHID.dpr
+++ Projects/Delphi/main/MainHID.dpr
@@ -3,6 +3,16 @@ program MainHID;
 uses
   Vcl.Forms,
   UnitHID in 'UnitHID.pas' {FormHID};
+  Hid in 'library\jvcl\run\Hid.pas',
+  JvHidControllerClass in 'library\jvcl\run\JvHidControllerClass.pas',
+  JvComponentBase in 'library\jvcl\run\JvComponentBase.pas',
+  JVCLVer in 'library\jvcl\run\JVCLVer.pas',
+  DBT in 'library\jvcl\run\DBT.pas',
+  JvSetupApi in 'library\jvcl\run\JvSetupApi.pas',
+  ModuleLoader in 'library\jvcl\run\ModuleLoader.pas',
+  WinConvTypes in 'library\jvcl\run\WinConvTypes.pas',
+  JvTypes in 'library\jvcl\run\JvTypes.pas',
+  JvResources in 'library\jvcl\run\JvResources.pas';
 
 {$R *.res}
 
